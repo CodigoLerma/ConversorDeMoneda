d00bacdcd4d042de34b36f71790c0c7d3c6e7a6e
Codigo Lerma <diego0303lc@gmail.com>
2025-05-05 04:13:28 -0600

    Clase ConsultarMoneda finalizada Y funcional

--- src/Moneda.java
+++ src/Moneda.java
@@ -2,5 +2,5 @@
 // ejemplo USD,MXN,1.00,19.00
 public record Moneda(String base_code, String target_code, double conversion_rate, double conversion_result) {
 
-
+    
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,10 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Principal {
+    public static void main(String[] args) {
+        ConsultarMoneda consulta = new ConsultarMoneda();
+
+        Moneda moneda = consulta.tipoDeMoneda("USD","MXN",10);
+        System.out.println(moneda.base_code());
+    }
 }
