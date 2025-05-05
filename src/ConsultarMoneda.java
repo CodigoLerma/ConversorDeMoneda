88521e9155ffd759e998e39253d4195731abdce0
Codigo Lerma <diego0303lc@gmail.com>
2025-05-05 02:43:11 -0600

    Se agregaron las solicitudes en la clase ConsultarMonda el http client y el http request

--- src/ConsultarMoneda.java
+++ src/ConsultarMoneda.java
@@ -1,3 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ConsultarMoneda {
 
+
+    public Moneda tipoDeMoneda(String monedaBase, String monedaObjetivo, double cantidad){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/76c58539963e7209d545ebde/pair/" + monedaBase + "/" + monedaObjetivo + "/" + cantidad );
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+    }
 }
--- src/Moneda.java
+++ src/Moneda.java
@@ -1,3 +1,6 @@
-public record Moneda(String base_code, String target_code, String conversion_rate, String conversion_result) {
+// guardara los datos obtenidos del json de monda base, moneda objetivo, cantidad a convertir y resultado de la convercion
+// ejemplo USD,MXN,1.00,19.00
+public record Moneda(String base_code, String target_code, double conversion_rate, double conversion_result) {
+
 
 }
