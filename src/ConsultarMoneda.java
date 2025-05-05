dcbf5bec8feb7c3be7753d7867802e667043320c
Codigo Lerma <diego0303lc@gmail.com>
2025-05-05 03:11:54 -0600

    Se implemento el http response

--- src/ConsultarMoneda.java
+++ src/ConsultarMoneda.java
@@ -1,6 +1,10 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultarMoneda {
 
@@ -13,6 +17,20 @@ public class ConsultarMoneda {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
                 .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(),Moneda.class);
+
+        }catch (IOException e){
+            throw new RuntimeException("Hubo un error de red o se alcanso el maximo de solicitudes" + e.getMessage());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException("La operacion fue interrumpida" + e.getMessage());
+        }
+
 
     }
 }
