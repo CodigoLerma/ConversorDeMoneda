928ccc4e58db94d9bd551f674f0a860c3e8d23ae
Codigo Lerma <diego0303lc@gmail.com>
2025-05-07 01:36:18 -0600

    Se implemento el while a la clase Principal

--- src/MostrarResultado.java
+++ src/MostrarResultado.java
@@ -1,10 +1,12 @@
 public class MostrarResultado {
 
 
-
+    // mostrara el resultado de las monedas seleccionadas
     public void resultado (String monedaBase, String monedaObjetivo, double cantidad){
         ConsultarMoneda consulta = new ConsultarMoneda();
-        Moneda moneda = consulta.tipoDeMoneda(monedaBase,monedaObjetivo,cantidad);
+        Moneda moneda = consulta.tipoDeMoneda(monedaBase,monedaObjetivo,cantidad); // enviara los datos de resultado para llamar a la api
+
+        //mostrar mensaje de la conversion realizada
         System.out.println("La conversion de: " + cantidad + " "+ "["+ monedaBase + "]");
         System.out.println("a " + "[" + monedaObjetivo + "]" + " es: " + moneda.conversion_result());
 
--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,40 @@
 import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
 
 // interfas para interactuar con el usuario
 public class Principal {
     public static void main(String[] args) {
+        double cantidad;
+        int opcion = 0;
+        Scanner lectura = new Scanner(System.in);
+
         MostrarResultado resultado = new MostrarResultado();
 
-        resultado.resultado("USD","MXN",1);
+        while (opcion != 7) {
+
+            System.out.println("""
+                    ***************************
+                     1: Dollar a pesos Mexicano
+                     2: Pesos Mexicano a Dollar
+                     3: Dollar a peso Argentino
+                     4: Peso Argentido a Dollar
+                     5: Dollar sol Peruano
+                     6: Sol Peruano a Dollar
+                     7: Cerrar la aplicacion
+                     **************************""");
+            opcion = lectura.nextInt();
+
+            
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
